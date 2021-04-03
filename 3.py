91851bd037097ae46e0a785468259bacd7e145ef
anik-1507103 <anikpramanikcse@gmail.com>
2021-04-03 23:08:37 +0600

    'second'

--- 3.py
+++ 3.py
@@ -29,8 +29,8 @@ node2=Node(2,node1)
 node3=Node(3,node1)
 node4=Node(4,node2)
         
-print(lca(node4,node3))
-print("Time Complexity: Suppose, the highest depth of any node in tree is n. \
+print("Result: ", lca(node4,node3))
+print("\nTime Complexity: Suppose, the highest depth of any node in tree is n. \
 Then time complexity is O(2n) in worst case. Because, first we store all n parents of node1, \
 which takes n unit. Then, we iterate over parents of node2 and lookup among parents of node1 until the ica is found. \
 In worst case, it takes n iteration. set() in python lookup time is O(1) in average. So, in total O(n+n*O(1))= O(2n) or O(n)")
